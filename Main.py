ba021de78a0f3f184c963edcf74f6c0bd83bed4e
cgiraldovi <72058741+cgiraldovi@users.noreply.github.com>
2021-10-25 19:04:39 -0500

    primer commit

---
+++ Main.py
@@ -0,0 +1,74 @@
+class Asiento:
+    def __init__(self,color,precio,registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+
+    def cambiarColor(cls, color):
+        if(color.equals("rojo")):
+            cls.color = color
+        elif(color.equals("verde")):
+            cls.color = color;
+        elif(color.equals("amarillo")):
+            cls.color = color
+        elif(color.equals("negro")):
+            cls.color = color
+        elif(color.equals("blanco")):
+            cls.color = "blanco"
+
+class Motor:
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+
+    def cambiarRegistro(cls, registro):
+        cls.registro = registro
+
+
+    def asignarTipo(cls,tipo):
+
+        if(tipo.equals("gasolina")):
+            cls.tipo = "gasolina"
+        elif(tipo.equals("electrico")):
+            cls.tipo = "electrico";
+
+
+
+class Auto:
+
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+        self.cantidadCreados = len(asientos)
+
+
+    def cantidadAsientos():
+        cont = 0
+        for asiento in self.asientos:
+            if(asiento != null):
+                cont = cont + 1
+        return cont
+
+    def verificarIntegridad():
+        cont = 0
+        contAsientos = 0;
+        for asiento in self.asientos:
+            if(asiento != null):
+                contAsientos = contAsientos + 1
+                if(asiento.registro == self.registro):
+                    if(self.registro == self.motor.registro):
+                        cont = cont + 1
+
+
+        if(cont == contAsientos):
+            return "Auto original"
+        else:
+            return "Las piezas no son originales"
+
+
+
